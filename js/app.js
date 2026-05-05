5071daf6b94aead2493185a70097abd4a8af9f30
Krister <kristervestland@hotmail.no>
2026-05-05 11:19:11 +0200

    Another tiny fix

--- js/app.js
+++ js/app.js
@@ -233,3 +233,11 @@ window.initTaskDrag = function(listEl) {
   listEl.querySelectorAll('.task-row').forEach(el => el.setAttribute('draggable', 'true'))
   makeDraggable(listEl, '.task-row')
 }
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.body.style.display = 'none'
+    requestAnimationFrame(() => {
+      document.body.style.display = ''
+    })
+  }, 100)
+})
